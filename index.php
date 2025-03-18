3aa8c2e04bd898b0816702d07c059767620ca238
Dang Van Doan 05 <dangvandoan0509@gmail.com>
2025-03-18 16:52:42 +0700

    update 1 18-03-2025

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="vietnam">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Web Code Tay</title>
+</head>
+<body>
+    <div class="khoingoaicung">
+        <div class="header">
+            <p>header</p>
+        </div>
+        <div class="menu">
+            <p>menu</p>
+        </div>
+        <div id="main">
+            <div class="sidebar">
+                Sidebar
+            </div>
+        </div>
+            <div class="footer">
+            <p>footer</p>
+        </div>
+    </div>
+
+</body>
+</html>
