54e7171cf81fa257234acc07c3cc9a05eef0f7a7
unknown <dangvandoan0509@gmail.com>
2025-03-22 21:51:26 +0700

    Update 3 22-03-2025

--- pages/sidebar/sidebar.php
+++ pages/sidebar/sidebar.php
@@ -1,8 +1,8 @@
 <div class="sidebar">
             <ul class="list_sidebar">
-                <li><a href="#">Ốp lưng</a></li>
-                <li><a href="#">Tai nghe</a></li>
-                <li><a href="#">Cục sạc</a></li>
+                <li><a href="index.php?quanly=danhmucsanpham&id=1">Ốp lưng</a></li>
+                <li><a href="index.php?quanly=danhmucsanpham&id=2">Tai nghe</a></li>
+                <li><a href="index.php?quanly=danhmucsanpham&id=3">Cục sạc</a></li>
                 <li><a href="#">Dán màn hình</a></li>
                 <li><a href="#">Gậy tự sướng</a></li>
                 <li><a href="#">Lót bàn phím</a></li>  
