7c191903fba937af22d16fff156b37a0da7fc927
Dang Van Doan 05 <dangvandoan0509@gmail.com>
2025-03-22 13:32:25 +0700

    Update 1 22-03-2025

---
+++ pages/main/index.php
@@ -0,0 +1,56 @@
+<h3>Sản phẩm mới nhất.</h3>
+                <ul class="product_list">
+                    <li>
+                        <a href="">
+                            <img src="images/anhDMBoVoiTuoi.jpg">
+                            <p class="title_product">Bộ vòi tưới</p>
+                            <p class="price_product">Giá:3000000 vnđ</p>
+                        </a>
+                    </li>
+                    <li>
+                        <img src="images/anhDMBoVoiTuoi.jpg">
+                        <p class="title_product">Bộ vòi tưới</p>
+                        <p class="price_product">Giá:3000000 vnđ</p>
+                    </li>
+                    <li>
+                        <img src="images/anhDMBoVoiTuoi.jpg">
+                        <p class="title_product">Bộ vòi tưới</p>
+                        <p class="price_product">Giá:3000000 vnđ</p>
+                    </li>
+                    <li>
+                        <img src="images/anhDMBoVoiTuoi.jpg">
+                        <p class="title_product">Bộ vòi tưới</p>
+                        <p class="price_product">Giá:3000000 vnđ</p>
+                    </li>
+                    <li>
+                        <img src="images/anhDMBoVoiTuoi.jpg">
+                        <p class="title_product">Bộ vòi tưới</p>
+                        <p class="price_product">Giá:3000000 vnđ</p>
+                    </li>
+                    <li>
+                        <img src="images/anhDMBoVoiTuoi.jpg">
+                        <p class="title_product">Bộ vòi tưới</p>
+                        <p class="price_product">Giá:3000000 vnđ</p>
+                    </li>
+                    <li>
+                        <img src="images/anhDMBoVoiTuoi.jpg">
+                        <p class="title_product">Bộ vòi tưới</p>
+                        <p class="price_product">Giá:3000000 vnđ</p>
+                    </li>
+                    <li>
+                        <img src="images/anhDMBoVoiTuoi.jpg">
+                        <p class="title_product">Bộ vòi tưới</p>
+                        <p class="price_product">Giá:3000000 vnđ</p>
+                    </li>
+                    <li>
+                        <img src="images/anhDMBoVoiTuoi.jpg">
+                        <p class="title_product">Bộ vòi tưới</p>
+                        <p class="price_product">Giá:3000000 vnđ</p>
+                    </li>
+                    <li>
+                        <img src="images/anhDMBoVoiTuoi.jpg">
+                        <p class="title_product">Bộ vòi tưới</p>
+                        <p class="price_product">Giá:3000000 vnđ</p>
+                    </li>
+                   
+                </ul>
