446cf81e3ad34abf8a58c20597c036ed3cc09460
unknown <dangvandoan0509@gmail.com>
2025-04-01 22:03:43 +0700

    Update 1 01-04-2025

---
+++ admincp/modules/menu.php
@@ -0,0 +1,7 @@
+<ul class="admincp_list">
+    <li><a href="index.php?action=quanlydanhmucsanpham">Quản lý danh mục sản phẩm</a></li>
+      <!-- action là tên của một biến. -->       
+    <li><a href="">Quản lý sản phẩm</a></li>
+    <li><a href="">Quản lý bài viết</a></li>
+    <li><a href="">Quản lý danh mục bài viết</a></li>
+</ul>
